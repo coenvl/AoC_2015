651f4d90f562aadfa662bf84813876c13ad592fe
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2025-01-09 12:00:20 +0100

    Fix 17 actually

--- src/day17.rs
+++ src/day17.rs
@@ -1,58 +1,30 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-
-/*fn __combine(containers: Vec<u8>, n: u8, cache: &mut HashMap<(Vec<u8>, u8), usize>) -> usize {
-    if let Some(&value) = cache.get(&(containers.clone(), n)) {
-        return value;
-    }
-    let mut count = 0;
-    for (i, &size) in containers.iter().enumerate() {
-        println!("{n}: container {size}");
-        if size > n {
-            continue
-        } else if size == n {
-            count += 1;
+fn combine(n: u8, list: &[u8], l: u8, lengths: &mut HashMap<u8, u16>) -> usize {
+    if n == 0 {
+        *lengths.entry(l).or_insert(0) += 1;
+        1
+    } else if list.len() == 0 {
+        0
+    } else {
+        let p1 = combine(n, &list[1..], l, lengths);
+        if list[0] <= n {
+            p1 + combine(n - list[0], &list[1..], l+1, lengths)
         } else {
-            let mut subset = containers.clone();
-            subset.remove(i);
-            count += combine(subset, n - size, cache);
+            p1
         }
     }
-    cache.insert((containers, n), count);
-    count
-}*/
-
-fn combine(containers: Vec<(u8, u8)>, n: u8) -> usize {
-    let mut stack : Vec<HashSet<(u8,u8)>> = containers.iter().map(|&x| HashSet::from([x])).collect();
-    let mut success: HashSet<Vec<(u8,u8)>> = HashSet::new();
-    while let Some(combo) = stack.pop() {
-        for &c in containers.iter() {
-            let mut next_combo = combo.clone();
-            if next_combo.insert(c) {
-                let total: u8 = next_combo.iter().map(|x| x.1 ).sum();
-                if total == n {
-                    let mut good: Vec<(u8, u8)> = next_combo.iter().map(|x| *x).collect();
-                    good.sort();
-                    success.insert(good);
-                } else if total < n {
-                    stack.push(next_combo);
-                }
-            }
-        }
-    }
-    println!("{:?}", success);
-    success.len()
 }
 
 pub fn day17(input: &str) -> (String, String) {
     // let input = "20\n15\n10\n5\n5";
-    let containers: Vec<(u8, u8)> = input.lines().enumerate()
-        .map(|(i,x)| (i as u8, x.parse().unwrap()))
+    let containers: Vec<u8> = input.lines()
+        .map(|x| x.parse().unwrap())
         .collect();
 
-    let part1 = combine(containers, 150); //, &mut cache);
+    let mut lengths: HashMap<u8, u16> = HashMap::new();
+    let part1 = combine(150, &containers, 0, &mut lengths);
+    let part2 = lengths.values().min().expect("No solution found");
 
-    // let part1 = 0;
-    let part2 = 0;
     (format!("{part1}"), format!("{part2}"))
 }
